a6d83391846e0160e8145c09ec71acf9485eca78
Garvit saini <sainigarvit275@gmail.com>
2026-01-17 02:08:46 +0530

    Updated GSAP animations and layout improvements

--- project1/index.js
+++ project1/index.js
@@ -1,3 +1,8 @@
+gsap.registerPlugin(SplitText);
+
+let split = new SplitText("#ctnt p", {
+  type: "chars,words,lines"
+});
 
 var t1 = gsap.timeline();
 
@@ -5,24 +10,24 @@ t1.from("#nav h3", {
     delay:0.1,
     y: -40,
     duration: 0.25,
-    stagger: 0.2,
+    stagger: 0.1,
     opacity: 0 
 });
 t1.from("#hi", {
     x: -50,
-    duration: 0.4,
+    duration: 0.3,
     opacity: 0,  
-    stagger: 0.25
+    stagger: 0.1
 });
 t1.from("#frcs", {
     x: 50,
-    duration: 0.4,
+    duration: 0.3,
     opacity: 0,   
-    stagger: 0.25
+    stagger: 0.1
 });
 t1.from("#gi", {
     x: -50,
-    duration: 0.4,
+    duration: 0.3,
     opacity: 0,   
     stagger: 0.1
 });
@@ -33,22 +38,22 @@ t1.from("#gi", {
 //     ease:"power2.inOut"
 // })
 t1.from("#img3", {
-    duration: 0.5,
+    duration: 0.25,
     rotate:170,
     opacity: 0,   
     stagger: 0.1
 });
 t1.from("#img2", {
-    duration: 0.5,
+    duration: 0.25,
     rotate:135,
     opacity: 0,   
     stagger: 0.1
 });
 t1.from("#img1", {
-    duration: 0.5,
+    duration: 0.25,
     rotate:90,
     opacity: 0,   
-    stagger: 0.1
+    stagger: 0.05
 });
 t1.from("#footer h3", {
     y: -40,
@@ -72,7 +77,7 @@ t1.from("#footer h3", {
 //     rotate:360,
 //     opacity: 0,   
 //     scale:0,
-//     scrollTrigger:{
+//     scrollTrigger:{ 
 //         trigger:"#p2",
 //         scroller:"body",
 //         // markers:true,
@@ -82,13 +87,13 @@ t1.from("#footer h3", {
 //     }
 // });
 t1.to("#page1 h1",{
-    transform:"translateX(-77%)",
+    transform:"translateX(-90%)",
     scrollTrigger:{
         trigger:"#page1",
         scroller:"body",
         start:"top 0%",
         end:"top -100%",
-        scrub:1,
+        scrub:3,
         pin:true
     }
 })
@@ -103,3 +108,73 @@ t1.to("#page2 img",{
         pin:true
     }
 })
+t1.to("#photu img",{
+    height:"80%",
+    filter: "grayscale(0%)",    
+    scrollTrigger:{
+        trigger:"#photu img",
+        scroller:"body",
+        start:"top 60%",
+        end:"top 10%",
+        scrub:5,
+        pin:false,
+    }
+})
+t1.from(split.chars,{
+  y:100,
+  opacity:0,
+  autoAlpha:0,
+  stagger:0.5,
+  scrollTrigger:{
+    trigger:"#ctnt",
+    scroller:"body",
+    start:"top 60%",
+    end:"top 25%",
+    scrub:5
+  }
+});
+t1.from("#lef > *",{
+  y:50,
+  opacity:0,
+  stagger:0.15,
+  scrollTrigger:{
+    trigger:"#last",
+    start:"top 70%",
+    end:"top 30%",
+    scrub:2
+  }
+});
+
+t1.from(".right .item",{
+  x:80,
+  opacity:0,
+  stagger:0.2,
+  scrollTrigger:{
+    trigger:"#last",
+    start:"top 70%",
+    end:"top 30%",
+    scrub:2
+  }
+});
+t1.from(".socials span",{
+  y:40,
+  opacity:0,
+  stagger:0.15,
+  scrollTrigger:{
+    trigger:"#last",
+    start:"top 60%",
+    end:"top 30%",
+    scrub:2
+  }
+});
+t1.from(".contact > div",{
+  y:40,
+  opacity:0,
+  stagger:0.2,
+  scrollTrigger:{
+    trigger:"#last",
+    start:"top 55%",
+    end:"top 25%",
+    scrub:2
+  }
+});
